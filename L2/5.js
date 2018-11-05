68519552d85d1c331cd08cb4e4bf98a6cf630617
Alex Dolmatova <doll.alex.by@gmail.comlist>
2018-11-05 22:54:48 +0300

    l2

--- L2/5.js
+++ L2/5.js
@@ -1,10 +1,8 @@
-/*function numberLine(num){ 
+function numberLine(num){ 
  console.log(0);
  for(var i=1;i<=Math.abs(num);i++){
   if(num<0) console.log(i*-1);
   else console.log(i);
  }
 }
-numberLine(-5);*/
-var a=0*-1;
-console.log(a);
+numberLine(-5);
