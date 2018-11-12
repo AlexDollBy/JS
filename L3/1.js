e6df9e277d195a82fd2413417771d3dd1cbd0449
Alex Dolmatova <doll.alex.by@gmail.comlist>
2018-11-12 22:52:55 +0300

    12 nov

---
+++ L3/1.js
@@ -0,0 +1,59 @@
+function checkUndefined(value){
+    return value===undefined;
+}
+
+function checkNull(value){
+    return value===null;
+}
+
+function checkNaN(value){
+    return value===NaN;
+}
+
+function checkEmptyObj(obj){
+    return Object.keys(obj).length === 0
+}
+
+function randomNum(min,max){
+    return (Math.random() * (max - min) + min).toFixed();
+}
+
+function powObj(obj){
+    for (var key in obj) {
+        obj[key] =  Math.pow(obj[key],2); 
+    }
+      return obj;
+}
+
+function deepEqual(a, b) {
+    if (a === b) {
+        return true;
+    }
+
+    if (a == null || typeof(a) != "object" ||
+        b == null || typeof(b) != "object"){
+        return false;
+    }
+
+    var propInA = 0, propInB = 0;
+    for (var property in a) {
+        propInA += 1;
+    }
+    for (var property in b) {
+        propInB += 1;
+        if (!(property in a) || !deepEqual(a[property], b[property])) {
+            return false;        
+        }
+    }        
+    return propInA == propInB;
+}
+
+function deleteIfFalse(obj){
+    for (var key in obj) {
+        if(!checkNull(obj[key])) delete obj[key]; 
+    }
+    return obj;
+}
+var obj = {here: {is:8}, object: null};
+console.log(deleteIfFalse(obj));
+
---
+++ L4/calculatorJS.js
@@ -0,0 +1,35 @@
+temp="";
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
+
+function getEl(){
+    text = document.getElementById("18");
+    textarea = document.getElementById("textarea");
+}
+function num(btn){
+  return !isNaN(btn);
+}
+
+function clearDisplay(){
+   text.value="0"; 
+   temp="";   
+}
+
+function clearHistory(){
+    textarea.value = "";
+    clearDisplay();
+}
+
+function display(symble){   
+    if(!num(symble) && !num(temp[temp.length-1])) temp = temp.replaceAt(temp.length-1,symble);
+    else temp+=symble;
+    text.value = temp;
+}
+
+function calculate(){
+    var result = eval(text.value);
+    text.value += "=" + result;
+    textarea.value += text.value +"\n";
+    temp = "" + result; 
+}
