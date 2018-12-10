ce542d54cfd3077548448cf4666e5a5a2e6a15c9
Alex Dolmatova <doll.alex.by@gmail.comlist>
2018-12-10 11:59:24 +0300

    10.12

--- l8/t2/index.js
+++ l8/t2/index.js
@@ -2,24 +2,20 @@
 const http = require("http");
 const path = require("path");
 
-const PORT = 3000;
-const STATUS_OK = 200;
-
+const port = 3000;
 const contentTypes = new Map([
   [".js", "text/javascript"],
   [".html", "text/html"],
-  [".css", "text/css"]
-]);
+  [".css", "text/css"]]);
 
-const requestHandler = (req, res) => {
-  const endpoint = req.url === "/" ? "/html/main.html" : req.url;
-  const localFilePath = path.join(__dirname,'/', endpoint);
-  fs.open(localFilePath, "r", (openErr, _) => {
-      fs.readFile(localFilePath, (readErr, data) => {
-      const extension = path.extname(localFilePath);
-      const contentType = contentTypes.get(extension);
-      console.log(contentTypes.get(extension));
-      res.writeHead(STATUS_OK, {
+const request = (req, res) => {
+  const end = req.url === "/" ? "/html/main.html" : req.url;
+  const FilePath = path.join(__dirname,'/', end);
+  fs.open(FilePath, "r", (err, _) => {
+      fs.readFile(FilePath, (err, data) => {
+      const ex = path.extname(FilePath);
+      const contentType = contentTypes.get(ex);
+      res.writeHead(200, {
         "Content-Type": contentType
       });
       res.end(data);
@@ -27,11 +23,10 @@ const requestHandler = (req, res) => {
   });
 };
 
-const server = http.createServer(requestHandler);
-
-server.listen(PORT, err => {
+const server = http.createServer(request);
+server.listen(port, err => {
   if (err) {
     console.error("Что-то пошло не так", err);
   }
-  console.log(`Сервер запущен. Порт: ${PORT}`);
+  console.log(`Сервер запущен. Порт: ${port}`);
 });
